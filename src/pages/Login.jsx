56ddeca017cd0fd63fdd6117d1d8bb03823271d9
Pugazhanthi11 <pugazhanthi.vk@gmail.com>
2026-06-06 22:24:16 +0530

    Refactor login component with new API endpoint
    Updated API endpoint for login functionality and improved error handling.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -28,7 +28,7 @@ useState(false);
 
       const response =
       await fetch(
-        "http://localhost:5000/api/auth/login",
+        "https://stemverseai1.onrender.com/api/auth/login",
         {
           method: "POST",
           headers: {
